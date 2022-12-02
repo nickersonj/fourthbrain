08123cd28781d5a20698c39c4af5ef3d451f6152
nickersonj <99698301+nickersonj@users.noreply.github.com>
2022-12-02 11:14:15 -0800

    completed week 6

--- assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
+++ assignments/week-06-supervised-ml/nb/tpot_electronics_pipeline.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +14,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 1.0
+# Average CV score on the training set was: 0.9925925925925926
 exported_pipeline = make_pipeline(
-    FastICA(tol=0.30000000000000004),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=2, min_samples_split=5, n_estimators=100)
+    StackingEstimator(estimator=SGDClassifier(alpha=0.01, eta0=0.01, fit_intercept=False, l1_ratio=0.5, learning_rate="constant", loss="perceptron", penalty="elasticnet", power_t=0.5)),
+    StackingEstimator(estimator=LinearSVC(C=0.5, dual=False, loss="squared_hinge", penalty="l2", tol=1e-05)),
+    DecisionTreeClassifier(criterion="entropy", max_depth=9, min_samples_leaf=4, min_samples_split=2)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
